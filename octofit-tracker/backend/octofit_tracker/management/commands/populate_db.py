2fedc791378de182dd5c7fd39d0fc1d77f282673
paveen99 <paveen.km@accenture.com>
2025-11-25 09:31:47 +0000

    Add management commands for populating the database with test data

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,39 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Workout, Leaderboard
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        # Delete existing data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Workout.objects.all().delete()
+        Leaderboard.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        ironman = User.objects.create(name='Iron Man', email='ironman@marvel.com', team=marvel)
+        captain = User.objects.create(name='Captain America', email='cap@marvel.com', team=marvel)
+        batman = User.objects.create(name='Batman', email='batman@dc.com', team=dc)
+        superman = User.objects.create(name='Superman', email='superman@dc.com', team=dc)
+
+        # Create activities
+        Activity.objects.create(user=ironman, type='Running', duration=30)
+        Activity.objects.create(user=captain, type='Cycling', duration=45)
+        Activity.objects.create(user=batman, type='Swimming', duration=60)
+        Activity.objects.create(user=superman, type='Weightlifting', duration=50)
+
+        # Create workouts
+        Workout.objects.create(name='Super Strength', description='Strength training for heroes', suggested_for='Marvel')
+        Workout.objects.create(name='Agility Boost', description='Agility training for DC heroes', suggested_for='DC')
+
+        # Create leaderboard
+        Leaderboard.objects.create(team=marvel, points=200)
+        Leaderboard.objects.create(team=dc, points=180)
+
+        self.stdout.write(self.style.SUCCESS('octofit_db populated with test data'))
